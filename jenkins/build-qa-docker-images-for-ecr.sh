575187a95004758ce0e83309ad5314bf391428db
“saitaa” <saitaacatkal@gmail.com>
2021-11-16 03:25:45 +0000

    fixed last

--- jenkins/build-qa-docker-images-for-ecr.sh
+++ jenkins/build-qa-docker-images-for-ecr.sh
@@ -1,4 +1,4 @@
- build --force-rm -t "${IMAGE_TAG_ADMIN_SERVER}" "${WORKSPACE}/spring-petclinic-admin-server"
+docker build --force-rm -t "${IMAGE_TAG_ADMIN_SERVER}" "${WORKSPACE}/spring-petclinic-admin-server"
 docker build --force-rm -t "${IMAGE_TAG_API_GATEWAY}" "${WORKSPACE}/spring-petclinic-api-gateway"
 docker build --force-rm -t "${IMAGE_TAG_CONFIG_SERVER}" "${WORKSPACE}/spring-petclinic-config-server"
 docker build --force-rm -t "${IMAGE_TAG_CUSTOMERS_SERVICE}" "${WORKSPACE}/spring-petclinic-customers-service"
